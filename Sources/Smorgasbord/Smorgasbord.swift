841e999d6d37df1f0801417b2da03c8e0b9be36b
Marko Rankovic <markocrush96@gmail.com>
2019-09-18 18:56:57 +0100

    multiplication of points

--- Sources/Smorgasbord/Smorgasbord.swift
+++ Sources/Smorgasbord/Smorgasbord.swift
@@ -17,6 +17,10 @@ extension Sequence {
 
 extension CGPoint {
     
+    public static func *(_ lhs: CGPoint, _ rhs: CGFloat) -> CGPoint {
+        return CGPoint(x: lhs.x * rhs, y: lhs.y * rhs)
+    }
+    
     public static func -(_ lhs: CGPoint, _ rhs: CGPoint) -> CGPoint {
         return CGPoint(x: lhs.x - rhs.x, y: lhs.y - rhs.y)
     }
@@ -24,40 +28,3 @@ extension CGPoint {
 }
 
 
-extension String {
-    
-    static func getString(from: Int, to: Int, string: String) -> String {
-        let arr: [Character] = Array(string)
-        var newArr: [Character] = []
-        for i in from..<to {
-            newArr.append(arr[i])
-        }
-        return arr.reduce(""){ "\($0)" + "\($1)" }
-    }
-
-    static func getStartIndexes(lhs: String, rhs: String) -> [Int] {
-        guard lhs.count <= rhs.count else {
-            return []
-        }
-        
-        var indexes: [Int] = []
-        for i in 0..<lhs.count {
-            if getString(from: i, to: i + rhs.count, string: rhs) == rhs {
-                indexes.append(i)
-            }
-        }
-        return indexes
-    }
-
-    public static func -(_ lhs: String, _ rhs: String) -> String {
-        let startIndexes = getStartIndexes(lhs: lhs, rhs: rhs)
-        var strings: [String] = []
-        var i = 0
-        for startIndex in startIndexes {
-            strings.append(getString(from: i, to: startIndex - 1, string: lhs))
-            i = startIndex + rhs.count + 1
-        }
-        return strings.reduce(""){ $0 + $1 }
-    }
-
-}
