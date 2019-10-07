ec8295536a77d7a05fa053dbd1c3a3883c790c8f
Marko Rankovic <markocrush96@gmail.com>
2019-10-07 18:04:15 +0100

    adding + operator for CGPoints

--- Sources/Smorgasbord/Smorgasbord.swift
+++ Sources/Smorgasbord/Smorgasbord.swift
@@ -23,6 +23,10 @@ extension CGPoint {
         return CGPoint(x: lhs.x - rhs.x, y: lhs.y - rhs.y)
     }
     
+    public static func +(_ lhs: CGPoint, _ rhs: CGPoint) -> CGPoint {
+        return CGPoint(x: lhs.x + rhs.x, y: lhs.y + rhs.y)
+    }
+    
 }
 
 extension GKScene {
